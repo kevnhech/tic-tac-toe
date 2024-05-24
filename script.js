ac7b3e0f85d1a73efc5bfbf9e5a950c1692253fc
kevnhech <kevnhech@gmail.com>
2024-05-24 15:21:07 -0700

    Store players in objects

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ const Gameboard = (function() {
   const board = ["", "", "",
                  "", "", "",
                  "", "", ""];
+
+  const playerOne = ((marker) => {
+    marker = "X";
+    return marker;
+  })();
+
+  const playerTwo = ((marker) => {
+    marker = "O";
+    return marker;
+  })();
 })();
