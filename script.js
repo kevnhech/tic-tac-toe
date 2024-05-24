1e4af95336cfdc4483967f02c0095ee506cfb7d6
kevnhech <kevnhech@gmail.com>
2024-05-24 15:22:44 -0700

    Create an object to control the flow of the game

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ const Gameboard = (function() {
     marker = "O";
     return marker;
   })();
+
+  const rounds = (() => {
+    while (board.filter(element => element == "").length > 0) {
+      playerOneIdx = prompt("PLAYER 1: Enter a number 1-9.") - 1;
+      board[playerOneIdx] = playerOne;
+
+      if (board.filter(element => element == "").length == 0) {
+        break;
+      }
+
+      playerTwoIdx = prompt("PLAYER 2: Enter a number 1-9.") - 1;
+      board[playerTwoIdx] = playerTwo;
+    }
+    console.log(board);
+   })();
 })();
