38f8d3f8208e405b7ed1b9e37678da38b80e3a29
kevin <kevnhech@gmail.com>
2024-05-27 17:37:44 -0700

    Make a win condition function

--- script.js
+++ script.js
@@ -3,28 +3,68 @@ const Gameboard = (function() {
                  "", "", "",
                  "", "", ""];
 
-  const playerOne = ((marker) => {
-    marker = "X";
-    return marker;
-  })();
+  const createPlayer = function(name, controller) {
+    this.name = name;
+    this.controller = controller;
+    return { name, controller };
+  };
 
-  const playerTwo = ((marker) => {
-    marker = "O";
-    return marker;
-  })();
+  const playerOne = createPlayer("PlayerX", "X");
+  const playerTwo = createPlayer("PlayerO", "O")
 
-  const rounds = (() => {
+  const winRound = () => {
+    this.x = (
+      [board[0], board[1], board[2]].filter(element => element == "X").length == 3 ||
+      [board[3], board[4], board[5]].filter(element => element == "X").length == 3 ||
+      [board[6], board[7], board[8]].filter(element => element == "X").length == 3 ||
+
+      [board[0], board[3], board[6]].filter(element => element == "X").length == 3 ||
+      [board[1], board[4], board[7]].filter(element => element == "X").length == 3 ||
+      [board[2], board[5], board[8]].filter(element => element == "X").length == 3 ||
+
+      [board[0], board[4], board[8]].filter(element => element == "X").length == 3 ||
+      [board[2], board[4], board[6]].filter(element => element == "X").length == 3
+    );
+
+    this.o = (
+      [board[0], board[1], board[2]].filter(element => element == "O").length == 3 ||
+      [board[3], board[4], board[5]].filter(element => element == "O").length == 3 ||
+      [board[6], board[7], board[8]].filter(element => element == "O").length == 3 ||
+
+      [board[0], board[3], board[6]].filter(element => element == "O").length == 3 ||
+      [board[1], board[4], board[7]].filter(element => element == "O").length == 3 ||
+      [board[2], board[5], board[8]].filter(element => element == "O").length == 3 ||
+
+      [board[0], board[4], board[8]].filter(element => element == "O").length == 3 ||
+      [board[2], board[4], board[6]].filter(element => element == "O").length == 3
+    );
+
+    return { x, o }
+  }
+
+  const playRound = (() => {
     while (board.filter(element => element == "").length > 0) {
       playerOneIdx = prompt("PLAYER 1: Enter a number 1-9.") - 1;
-      board[playerOneIdx] = playerOne;
+      board[playerOneIdx] = playerOne.controller;
+      console.log(board);
 
-      if (board.filter(element => element == "").length == 0) {
+      if (winRound().x) {
+        console.log("Player 1 wins!")
+        break;
+      } else if (board.filter(element => element == "").length == 0 && winRound().x == false && winRound().o == false) {
+        console.log("Tie!");
         break;
       }
 
       playerTwoIdx = prompt("PLAYER 2: Enter a number 1-9.") - 1;
-      board[playerTwoIdx] = playerTwo;
+      board[playerTwoIdx] = playerTwo.controller;
+      console.log(board);
+
+      if (winRound().o) {
+        console.log("Player 2 wins!")
+        break;
+      }
     }
-    console.log(board);
+    // console.log(board);
    })();
 })();
